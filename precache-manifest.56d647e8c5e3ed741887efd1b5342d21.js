8b0b2823ac47fe0a575c3b72712531792092cc8b
dariuszb <greedo904@gmail.com>
2020-05-14 21:49:19 +0200

    Updates

---
+++ precache-manifest.56d647e8c5e3ed741887efd1b5342d21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8566ec67de46c947baeb749b9e99deac",
+    "url": "/Car-dealer/index.html"
+  },
+  {
+    "revision": "ed21bc20edc7c015deed",
+    "url": "/Car-dealer/static/css/main.694b4c22.chunk.css"
+  },
+  {
+    "revision": "f463824c67b587ba76bf",
+    "url": "/Car-dealer/static/js/2.c04cf3a5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Car-dealer/static/js/2.c04cf3a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed21bc20edc7c015deed",
+    "url": "/Car-dealer/static/js/main.83acd238.chunk.js"
+  },
+  {
+    "revision": "f4ab37e2e5a02422afae",
+    "url": "/Car-dealer/static/js/runtime-main.b222779f.js"
+  },
+  {
+    "revision": "40bc833464964a312f07bb97927665e7",
+    "url": "/Car-dealer/static/media/photo-of-person-driving-769892.40bc8334.jpg"
+  }
+]);
