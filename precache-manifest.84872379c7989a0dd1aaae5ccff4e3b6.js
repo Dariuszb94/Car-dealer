e3954c119217aa70ad4696646f298a4fe231bde5
dariuszb <greedo904@gmail.com>
2020-05-29 17:26:30 +0200

    Updates

--- precache-manifest.84872379c7989a0dd1aaae5ccff4e3b6.js
+++ precache-manifest.84872379c7989a0dd1aaae5ccff4e3b6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "39835c66a10b510d85dd6a741d56dfc8",
+    "revision": "5d1a6f62fab74bd2688ee3d7732b1de8",
     "url": "/Car-dealer/index.html"
   },
   {
-    "revision": "9339b58d13d617535f89",
+    "revision": "90e35bb1b3aa01c906ac",
     "url": "/Car-dealer/static/css/main.a53f28f0.chunk.css"
   },
   {
-    "revision": "6b1d399ff5cbe882b938",
-    "url": "/Car-dealer/static/js/2.820e6e12.chunk.js"
+    "revision": "115407f07e45d3c942ac",
+    "url": "/Car-dealer/static/js/2.0f9b2e51.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/Car-dealer/static/js/2.820e6e12.chunk.js.LICENSE.txt"
+    "url": "/Car-dealer/static/js/2.0f9b2e51.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9339b58d13d617535f89",
-    "url": "/Car-dealer/static/js/main.d70bbdc3.chunk.js"
+    "revision": "90e35bb1b3aa01c906ac",
+    "url": "/Car-dealer/static/js/main.a87fe905.chunk.js"
   },
   {
     "revision": "f4ab37e2e5a02422afae",
